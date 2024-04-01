e12ca61e695fe41bd1c0cce8b602cf4ee04e7dc7
memset0 <memset0@outlook.com>
2024-04-01 16:21:19 +0800

    bug fixes & update

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Dynamic DNS script for the LAN of Zhejiang University, working with DNSPOD API v
 ### Usage
 
 太困了，明天补
+
+### Configuration
+
+#### secret
+
+DNSPOD API 的 secret，请自行在腾讯云后台获取。
+
+#### keyword
+
+脚本用于检索 IPv4 连接的关键词，如果你使用学校的拨号上网工具，这项可以为 `SRun3K专用宽带拨号`，如果你使用 ZJU Connect 等第三方工具，这项应该为 `ZJUVPN` 或你自己设定的 VPN 名称。
+
+具体查看方法为，在终端输入 `ipconfig` 指令，找到某个 `210/222.*.*.*` 的 IPv4 地址，查看其对应的链接名称。如果使用学校的拨号上网工具，这项可能为 `PPP 适配器 SRun3K专用宽带拨号连接`，如果使用 ZJU Connect 工具，这项可能为 `PPP 适配器 ZJUVPN`，只需要从中截取一个能唯一确定连接的关键词填入配置项即可。
--- ddns.py
+++ ddns.py
@@ -1,3 +1,5 @@
+import argparse
+import os
 import json
 import yaml
 from os import path, popen
@@ -7,12 +9,8 @@ from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.dnspod.v20210323 import dnspod_client, models
 
-config_dir = path.join(path.dirname(__file__), './config.yml')
-with open(config_dir, 'r+', encoding='utf8') as file:
-    config_text = file.read()
-    config = yaml.load(config_text, Loader=yaml.SafeLoader)
-    file.close()
-print('config:', config)
+config = {}
+cache_dir = ''
 
 
 def DescribeRecordList(params):
@@ -48,6 +46,7 @@ def ModifyRecord(params):
 
 
 def update(host_ip):
+    print('update:', host_ip)
     record_list = DescribeRecordList({"Domain": config['domain']})['RecordList']
     for record in record_list:
         if record['Name'] in config['subdomain']:
@@ -65,7 +64,6 @@ def update(host_ip):
 
 
 def get_cache():
-    cache_dir = path.join(path.dirname(__file__), './cache.txt')
     if not path.exists(cache_dir):
         return ''
     with open(cache_dir, 'r+', encoding='utf8') as file:
@@ -73,7 +71,6 @@ def get_cache():
 
 
 def set_cache(new_cache):
-    cache_dir = path.join(path.dirname(__file__), './cache.txt')
     with open(cache_dir, 'w+', encoding='utf8') as file:
         file.write(new_cache)
 
@@ -94,8 +91,31 @@ def get_host_ip():
     return ''
 
 
+def load_config():
+    global config, cache_dir
+    parser = argparse.ArgumentParser(description='Process some configurations.')
+    parser.add_argument('-c', '--config', help='Specify the location of the configuration file.', default='config.yml')
+    args = parser.parse_args()
+
+    config_dir = args.config if os.path.isabs(args.config) else os.path.join(os.getcwd(), args.config)
+    if not os.path.exists(config_dir):
+        print('config.yml not found!')
+        input('Press Enter to exit...')
+        return
+
+    cache_dir = os.path.abspath(os.path.join(os.path.dirname(config_dir), './cache.txt'))
+    with open(config_dir, 'r+', encoding='utf8') as file:
+        config_text = file.read()
+        config = yaml.load(config_text, Loader=yaml.SafeLoader)
+        file.close()
+    print('config:', config)
+
+
 if __name__ == '__main__':
+    load_config()
     host_ip = get_host_ip()
     if host_ip != get_cache():
-        set_cache(host_ip)
         update(host_ip)
+        set_cache(host_ip)
+    else:
+        print('Cache hit, skipped!')
